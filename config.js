5a671344ba44368e3176687f7507b8ce5bbdc934
asmagin1 <andrei@dodopizza.ee>
2026-04-23 16:12:32 +0300

    Update SUPABASE_URL in config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.APP_CONFIG = {
-  SUPABASE_URL: "https://vjnosyuqttdvlrrurebb.supabase.co/rest/v1/",
+  SUPABASE_URL: "https://vjnosyuqttdvlrrurebb.supabase.co",
   SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZqbm9zeXVxdHRkdmxycnVyZWJiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY5NDA0NzksImV4cCI6MjA5MjUxNjQ3OX0.L7epqtf9LYswPalvBsPewaxKa7YpYYFnl8-s2L_RFd4"
 };
