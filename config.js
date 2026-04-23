4ebbe1791c471a966de493d49847fb43727beb12
asmagin1 <andrei@dodopizza.ee>
2026-04-23 15:58:55 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,4 @@
-```javascript
 window.APP_CONFIG = {
-  DATA_MODE: 'supabase',
-  SUPABASE_URL: 'https://vjnosyuqttdvlrrurebb.supabase.co/rest/v1/',
-  SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZqbm9zeXVxdHRkdmxycnVyZWJiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY5NDA0NzksImV4cCI6MjA5MjUxNjQ3OX0.L7epqtf9LYswPalvBsPewaxKa7YpYYFnl8-s2L_RFd4',
-  SUPABASE_STORAGE_BUCKET: 'generated-files'
+  SUPABASE_URL: "https://vjnosyuqttdvlrrurebb.supabase.co/rest/v1/",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZqbm9zeXVxdHRkdmxycnVyZWJiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY5NDA0NzksImV4cCI6MjA5MjUxNjQ3OX0.L7epqtf9LYswPalvBsPewaxKa7YpYYFnl8-s2L_RFd4"
 };
-```
