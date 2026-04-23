ec8bf983352a5092bad7f0a33b20e74e84733332
asmagin1 <andrei@dodopizza.ee>
2026-04-23 15:37:25 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,8 @@
+```javascript
 window.APP_CONFIG = {
-  DATA_MODE: 'local',
-  SUPABASE_URL: '',
-  SUPABASE_ANON_KEY: '',
+  DATA_MODE: 'supabase',
+  SUPABASE_URL: 'https://supabase.com/dashboard/project/vjnosyuqttdvlrrurebb',
+  SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZqbm9zeXVxdHRkdmxycnVyZWJiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY5NDA0NzksImV4cCI6MjA5MjUxNjQ3OX0.L7epqtf9LYswPalvBsPewaxKa7YpYYFnl8-s2L_RFd4',
   SUPABASE_STORAGE_BUCKET: 'generated-files'
 };
+```
